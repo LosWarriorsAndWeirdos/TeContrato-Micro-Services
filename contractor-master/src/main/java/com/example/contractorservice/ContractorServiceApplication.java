2890729c248bb99ffb7cdd46f2f74cb80b591a9f
einenekkieinmana <90997198+einenekkieinmana@users.noreply.github.com>
2022-06-18 14:30:04 -0500

    project-til-gateway

--- contractor-master/src/main/java/com/example/contractorservice/ContractorServiceApplication.java
+++ contractor-master/src/main/java/com/example/contractorservice/ContractorServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.contractorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ContractorServiceApplication {
 
 	public static void main(String[] args) {
