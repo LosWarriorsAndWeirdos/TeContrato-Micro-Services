54f4f7f7747553c7cb8fba47ea5f9f6e1c22d5a1
einenekkieinmana <90997198+einenekkieinmana@users.noreply.github.com>
2022-06-17 20:55:42 -0500

    eureka

--- config-service/src/main/java/com/example/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/example/configservice/ConfigServiceApplication.java
@@ -2,12 +2,12 @@ package com.example.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
 @SpringBootApplication
-public class ConfigServiceApplication {
-
+@EnableConfigServer
+public class ConfigServiceApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServiceApplication.class, args);
     }
--- config-service/src/test/java/com/example/configservice/ConfigServiceApplicationTests.java
+++ config-service/src/test/java/com/example/configservice/ConfigServiceApplicationTests.java
@@ -8,6 +8,7 @@ class ConfigServiceApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
